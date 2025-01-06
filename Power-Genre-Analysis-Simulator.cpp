d10476f2308688c33ccd08a46c09a8d465f324a5
Randomzo <yemehidogho@gmail.com>
2025-01-06 15:10:36 +0000

    The start of the project

---
+++ Power-Genre-Analysis-Simulator.cpp
@@ -0,0 +1,37 @@
+// A simple program where users build teams based on elemental affinities, analyze win probabilities, and battle using randomized outcomes. 
+//                                 Starting small but will expand with advanced mechanics over time :9.
+#include <format>
+#include <iostream>
+#include <string>
+using namespace std;
+
+// class for the types 
+// Elami is Estako for animals 
+class Elami 
+{
+    private:
+        string powerGenre;
+        string name;
+
+    public:
+        int health;
+
+    
+    Elami(int hearts, const string& fam, const string& label):
+         health(hearts), powerGenre(fam), name(label)  {
+        
+}
+};
+/* Each genre will have a unique ability linked to two for balance reasons and a negetive (only when fighting counter )
+ fire double damage,
+ earth double health,
+water half damage intake,
+ air double postive odds
+ fusion types have half of each benfit  so 12 Elami */ 
+
+
+
+int main () {
+
+    return {};
+}
